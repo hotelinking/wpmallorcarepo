daf87a996031412649d17c9dd6751021cbdcca15
Xisco Llado <x.llado@hotelinking.com>
2017-04-15 18:14:14 +0200

    generate base css files

--- wp-content/themes/wp-mallorca/gulpfile.js
+++ wp-content/themes/wp-mallorca/gulpfile.js
@@ -6,16 +6,25 @@ var uglify = require('gulp-uglify');
 var pump = require('pump');
 var imagemin = require('gulp-imagemin');
 var rename = require('gulp-rename');
+var clean = require('gulp-clean');
+var concat = require('gulp-concat');
 
- //Compile sass files
- gulp.task('sass', function () {
- 	return gulp.src('./sass/**/*.scss')
- 	.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
- 	.pipe(gulp.dest('./style.css'));
- });
+//Compile sass files
+gulp.task('sass',['clean_css'], function () {
+	return gulp.src('./sass/*.scss')
+	.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+	.pipe(concat('style.css'))
+	.pipe(gulp.dest('./'));
+});
+
+//Clean style.css
+gulp.task('clean_css', function () {
+	return gulp.src('./style.css', {read: false})
+	.pipe(clean());
+});
 
 //Compile JS files
-gulp.task('js', function (cb) {
+gulp.task('js', ['clean_js'],function (cb) {
 	pump([
 		gulp.src('./js/**/*.js'),
 		uglify()
@@ -26,6 +35,12 @@ gulp.task('js', function (cb) {
 	);
 });
 
+//Clean js
+gulp.task('clean_js', function () {
+	return gulp.src('./js/*.min.js', {read: false})
+	.pipe(clean());
+});
+
 //Compile images
 gulp.task('img', () =>
 	gulp.src('./img/**/*.*')
@@ -33,15 +48,16 @@ gulp.task('img', () =>
 	.pipe(gulp.dest('./img'))
 	);
 
-//Create dist folder
+//Files and directories to add to distribution theme folder
 var filesToMove = [
 	'./partials/**/*.*',
 	'./img/**/*.*',
-	'./js/**/*.*',
+	'./js/**/*.min.js',
 	'./*.php',
 	'./style.css',
 ];
 
+//Create distribution theme folder
 gulp.task('dist',['sass','js','img'], function(){
 	// the base option sets the relative root for the set of files,
 	// preserving the folder structure
@@ -49,6 +65,7 @@ gulp.task('dist',['sass','js','img'], function(){
 	.pipe(gulp.dest('dist'));
 });
 
+//Watch for changes
 gulp.task('watch', function () {
 	gulp.watch('./sass/**/*.scss', ['sass']);
 	gulp.watch('./js/**/*.js', ['js']);
