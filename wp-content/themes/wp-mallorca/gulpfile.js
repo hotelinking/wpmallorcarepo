bb7b2828e38154a880b434c83098f6780edab780
Xisco Llado <x.llado@hotelinking.com>
2017-04-15 17:03:43 +0200

    modify gulp file

--- wp-content/themes/wp-mallorca/gulpfile.js
+++ wp-content/themes/wp-mallorca/gulpfile.js
@@ -1,36 +1,52 @@
 'use strict';
- 
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 var pump = require('pump');
 const imagemin = require('gulp-imagemin');
- 
+
  //Compile sass files
-gulp.task('sass', function () {
+ gulp.task('sass', function () {
   return gulp.src('./src/sass/**/*.scss')
-     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest('./style.css'));
+  .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+  .pipe(gulp.dest('./style.css'));
 });
 
 //Compile JS files
 gulp.task('js', function (cb) {
   pump([
-        gulp.src('./src/js/**/*.js'),
-        uglify(),
-        gulp.dest('./js')
+    gulp.src('./src/js/**/*.js'),
+    uglify(),
+    gulp.dest('./js')
     ],
     cb
-  );
+    );
 });
 
 //Compile images
 gulp.task('img', () =>
-    gulp.src('./src/img/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('./img'))
-);
- 
+  gulp.src('./src/img/*')
+  .pipe(imagemin())
+  .pipe(gulp.dest('./img'))
+  );
+
+//Create dist folder
+var filesToMove = [
+'./partials/**/*.*',
+'./img/**/*.*',
+'./js/**/*.*',
+'./*.php',
+'./style.css',
+];
+
+gulp.task('dist', function(){
+  // the base option sets the relative root for the set of files,
+  // preserving the folder structure
+  gulp.src(filesToMove, { base: './' })
+  .pipe(gulp.dest('dist'));
+});
+
 gulp.task('watch', function () {
   gulp.watch('./src/sass/**/*.scss', ['sass']);
   gulp.watch('./src/js/**/*.js', ['js']);
