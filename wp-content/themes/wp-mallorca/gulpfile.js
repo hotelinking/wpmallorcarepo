604f041f76a22c76fc045f10c4dbf4bb521b61ef
Xisco Llado <x.llado@hotelinking.com>
2017-04-15 10:32:25 +0200

    add gulp functionality

---
+++ wp-content/themes/wp-mallorca/gulpfile.js
@@ -0,0 +1,38 @@
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+const imagemin = require('gulp-imagemin');
+ 
+ //Compile sass files
+gulp.task('sass', function () {
+  return gulp.src('./src/sass/**/*.scss')
+     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest('./style.css'));
+});
+
+//Compile JS files
+gulp.task('js', function (cb) {
+  pump([
+        gulp.src('./src/js/**/*.js'),
+        uglify(),
+        gulp.dest('./js')
+    ],
+    cb
+  );
+});
+
+//Compile images
+gulp.task('img', () =>
+    gulp.src('./src/img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./img'))
+);
+ 
+gulp.task('watch', function () {
+  gulp.watch('./src/sass/**/*.scss', ['sass']);
+  gulp.watch('./src/js/**/*.js', ['js']);
+  gulp.watch('./src/img/', ['img']);
+});
